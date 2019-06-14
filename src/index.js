9fee2f5ba4a77f2f0c0bdadb8914ea49c5b8b9cd
3014zhangshuo <3014zhangshuo@gmail.com>
2019-06-14 21:57:08 +0800

    修改共享状态

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let appState = {
+  title: {
+    text: "A React Litte Book",
+    color: "red"
+  },
+  content: {
+    text: "React.js Content",
+    color: "blue"
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function dispatch(action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      state.title.text = action.text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      state.title.color = action.color
+      break
+    default:
+      break
+  }
+}
+
+function renderApp(appState) {
+  renderTitle(appState.title)
+  renderContent(appState.content)
+}
+
+function renderTitle(title) {
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = title.text
+  titleDOM.style.color = title.color
+}
+
+function renderContent(content) {
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = content.text
+  contentDOM.style.color = content.color
+}
+
+
+renderApp(appState)
+
+dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React 小书》' })
+dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' })
+renderApp(appState)
